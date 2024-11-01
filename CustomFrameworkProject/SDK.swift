284fac8abf60b95e33c5fbdbc9dc9b35485e2b23
Martin Tarres <martintarres@gmail.com>
2024-11-01 14:17:21 -0300

    swift package added

--- CustomFrameworkProject/SDK.swift
+++ CustomFrameworkProject/SDK.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Martin Tarres on 01/11/2024.
 //
-
-import Foundation
-
-struct SDK{
-    static func doSomework(){
-        print("Doing some work")
-    }
-}
+//
+//import Foundation
+//
+//struct SDK{
+//    static func doSomework(){
+//        print("Doing some work")
+//    }
+//}
